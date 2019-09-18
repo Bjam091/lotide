513ef46ecd1a583ee74d52396f4343377375a040
Brandon <brandonjamiller@hotmail.com>
2019-09-18 02:40:41 +0000

    eqArrays.js with updated code

--- eqArrays.js
+++ eqArrays.js
@@ -7,7 +7,7 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(a, b){
-  if (a === b) return true;
+  if (a.length !== b.length) return false;
   for (var i = 0; i < a.length; ++i) {
   if (a[i] !== b[i]) return false;
   }
