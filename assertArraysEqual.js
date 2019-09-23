e8ffb47f199717348acc8d32c03fb025677dfede
Brandon <brandonjamiller@hotmail.com>
2019-09-23 21:30:31 +0000

    assertArraysEqual.js with refactored code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,10 @@
 const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected) === false) {
-   return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
-  };   
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) 
-  };
-
-
-const eqArrays = function(a, b){
-  if (a.length !== b.length) return false;
-  for (var i = 0; i < a.length; ++i) {
-  if (a[i] !== b[i]) return false;
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
-  return true;
-}
+  return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
 
 
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+module.exports = assertArraysEqual;
