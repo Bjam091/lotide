ef9527a20b280631aaa5689afc29a01778e510f9
Brandon <brandonjamiller@hotmail.com>
2019-09-23 20:44:33 +0000

    head.js with refactored code

--- head.js
+++ head.js
@@ -1,17 +1,9 @@
 // FUNCTION IMPLEMNTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 var head = function (paramater) {
   return paramater[0];
 };
 
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
+module.exports = head;
