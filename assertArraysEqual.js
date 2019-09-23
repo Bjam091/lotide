b6a10053a02040e45ce126fc100c624ba414158d
Brandon <brandonjamiller@hotmail.com>
2019-09-23 21:56:22 +0000

    assertArraysEqual.js with refactored code, completed

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,11 @@
+const eqArrays = require('./eqArrays')
+
 const assertArraysEqual = function(actual, expected) {
-  if (actual === expected) {
+  if (eqArrays(actual, expected) === true) {
     return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
   return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
-
 
 
 module.exports = assertArraysEqual;
