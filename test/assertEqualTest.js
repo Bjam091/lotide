2eb6b4e9ff7be46465feadb0bcdadc4fca3e4163
Brandon <brandonjamiller@hotmail.com>
2019-09-23 20:41:46 +0000

    assertEqualTest.js examples for refactored code

---
+++ test/assertEqualTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require('../assertEqual');
+
+
+
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("Happy Smiley Face", "Happy Smiley Face");
+assertEqual(1,9);
