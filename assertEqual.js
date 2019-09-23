d47b0897eafb6ec5866d588cd5281002c047542f
Brandon <brandonjamiller@hotmail.com>
2019-09-23 20:40:46 +0000

    assertEqual.js with refactorng

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,13 @@
 // FUNCTION IMPLEMNTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
+  return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual("Happy Smiley Face", "Happy Smiley Face");
-assertEqual(1,9);
+
+
+
+
+module.exports = assertEqual;
