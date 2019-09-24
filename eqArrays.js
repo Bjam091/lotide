1230c5ec041eaf1109caf2b3af7d0338a24408e9
Brandon <brandonjamiller@hotmail.com>
2019-09-24 01:46:47 +0000

    eqArrays.js with completed code

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,10 @@
-
-
-const eqArrays = function(a, b){
+const eqArrays = function(a, b) {
   if (a.length !== b.length) return false;
-  for (var i = 0; i < a.length; ++i) {
-  if (a[i] !== b[i]) return false;
+  for (let i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
   }
   return true;
-}
+};
 
 
 module.exports = eqArrays;
