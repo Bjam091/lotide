a7024792786197828bfb0650e19090cec5664bb6
Brandon <brandonjamiller@hotmail.com>
2019-09-24 01:51:08 +0000

    takeUntil.js with completed code

--- takeUntil.js
+++ takeUntil.js
@@ -1,21 +1,3 @@
-const assertArraysEqual = function(actual, expected) {
-  const same = eqArrays(actual, expected);
-  if (same) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-  return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
-
-const eqArrays = function(a, b) {
-  if (a.length !== b.length) return false;
-  for (let i = 0; i < a.length; ++i) {
-    if (a[i] !== b[i]) return false;
-  }
-  return true;
-};
-
-
 const takeUntil = function(array, callback) {
   for (let item in array) {
     if (callback(array[item])) {
@@ -26,17 +8,4 @@ const takeUntil = function(array, callback) {
 };
 
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-
-assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+module.exports = takeUntil;
