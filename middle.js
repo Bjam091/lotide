3e06aaf8b70efd9b013aadd1fd0dde0ddf409d68
Brandon <brandonjamiller@hotmail.com>
2019-09-24 01:49:59 +0000

    middle.js with completed code

--- middle.js
+++ middle.js
@@ -1,16 +1,15 @@
-
-const middle = function(a){
-  let newArray = []
-  if( a.length <= 2){
+const middle = function(a) {
+  let newArray = [];
+  if (a.length <= 2) {
   } else if ((a.length - 1) % 2 === 0) {
     newArray.push(a[Math.floor((a.length / 2))]);
   } else {
     newArray.push(a.length / 2);
-    newArray.push((a.length / 2) +1);
+    newArray.push((a.length / 2) + 1);
   }
   
-return newArray
-}
+  return newArray;
+};
 
 
 module.exports = middle;
