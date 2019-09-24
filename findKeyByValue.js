dc18797f184a5e98ad2890708461aefb0767ad90
Brandon <brandonjamiller@hotmail.com>
2019-09-24 01:49:46 +0000

    findKeyByValue.js with completed code

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-  return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
-
 const findKeyByValue = function(shows, value) {
   for (let genre in shows) {
     if (shows[genre] === value) {
@@ -14,18 +6,4 @@ const findKeyByValue = function(shows, value) {
   } return undefined;
 };
 
-
-
-
-const bestTVShowsByGenre = {
-  sciFi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire",
-  animation: "Bob's Burgers",
-  reality: "Big Brother"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Big Brother"), "reality");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Law and Order"), undefined);
+module.exports = findKeyByValue;
