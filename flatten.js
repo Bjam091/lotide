087e138d3aaaf20d1d8008a4cba307ae0eb529b1
Brandon <brandonjamiller@hotmail.com>
2019-09-24 01:48:21 +0000

    flatten.js with completed code

--- flatten.js
+++ flatten.js
@@ -1,25 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-  return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+const flatten = function(arr) {
+  let newArray = [].concat.apply([],arr);
+  return newArray;
 };
 
-
-// const eqArrays = function(a, b){
-//   if (a.length !== b.length) return false;
-//   for (var i = 0; i < a.length; ++i) {
-//   if (a[i] !== b[i]) return false;
-//   }
-//   return true;
-// }
-
-
-const flatArray = function(arr){
-  let newArray = [].concat.apply([],arr)
-  return newArray
-
-}
-//console.log(flatArray((([1, 2, [3, 4], 5, [6]])), ([1, 2, 3, 4, 5, 6])))
-
-assertEqual((flatArray(([1, 2, [3, 4], 5, [6]]))), ([1, 2, 3, 4, 5, 6])) // => [1, 2, 3, 4, 5, 6]
+module.exports = flatten;
